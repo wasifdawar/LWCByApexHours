cf3da890f7a911c148e716624bf08d8dee70307b
Wasif Dawar <m.wasifdawar@gmail.com>
2024-01-16 21:24:02 +0500

    Dynamic data by input field

--- force-app/main/default/lwc/bindHTMLDynamic/bindHTMLDynamic.js
+++ force-app/main/default/lwc/bindHTMLDynamic/bindHTMLDynamic.js
@@ -1,3 +1,9 @@
 import { LightningElement } from 'lwc';
 
-export default class BindHTMLDynamic extends LightningElement {}
+export default class BindHTMLDynamic extends LightningElement {
+    myValue = "Salesforce by M Wasif Dawar";
+    handleChange(event)
+    {
+        this.myValue = event.target.value;
+    }
+}
