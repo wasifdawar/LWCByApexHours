b836bb18a7ffb435a56cc5be43ecf4fc35c7d89a
Wasif Dawar <m.wasifdawar@gmail.com>
2024-01-17 14:30:55 +0500

    Testing Constructor , Connected and disconnected Callbacks on console logs

--- force-app/main/default/lwc/lifecycleHooks/lifecycleHooks.js
+++ force-app/main/default/lwc/lifecycleHooks/lifecycleHooks.js
@@ -1,3 +1,20 @@
 import { LightningElement } from 'lwc';
 
-export default class LifecycleHooks extends LightningElement {}
+export default class LifecycleHooks extends LightningElement 
+{
+  constructor()
+  {
+    super();
+    console.log("Constructor");
+  }
+
+  connectedCallback()
+  {
+    console.log("Connected CallBack");
+  }
+
+  disconnectedCallback()
+  {
+    console.log("Disconnected CallBack");
+  }
+}
