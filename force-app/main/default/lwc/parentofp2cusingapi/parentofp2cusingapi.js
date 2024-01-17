31670adeb5989ee29fa314436b3ac30342985d70
Wasif Dawar <m.wasifdawar@gmail.com>
2024-01-18 02:52:50 +0500

    Passing the value using API property

--- force-app/main/default/lwc/parentofp2cusingapi/parentofp2cusingapi.js
+++ force-app/main/default/lwc/parentofp2cusingapi/parentofp2cusingapi.js
@@ -1,3 +1,10 @@
 import { LightningElement } from 'lwc';
 
-export default class Parentofp2cusingapi extends LightningElement {}
+export default class Parentofp2cusingapi extends LightningElement {
+
+    percentage = 20;
+
+    handleOnChange(event){
+        this.percentage = event.target.value();
+    }
+}
