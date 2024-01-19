f2aacbd457bdbd385692f3e0ab4c10c068ddc809
Wasif Dawar <m.wasifdawar@gmail.com>
2024-01-19 23:56:13 +0500

    componnet setup

--- force-app/main/default/lwc/childComponentCommunication/childComponentCommunication.js
+++ force-app/main/default/lwc/childComponentCommunication/childComponentCommunication.js
@@ -1,3 +1,8 @@
 import { LightningElement } from 'lwc';
 
-export default class ChildComponentCommunication extends LightningElement {}
+export default class ChildComponentCommunication extends LightningElement {
+    handleOnClick()
+    {
+     
+}
+}
