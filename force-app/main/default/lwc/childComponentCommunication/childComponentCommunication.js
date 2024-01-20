cd7026aaeee8dd3790863736f3ab005e0dae2f77
Wasif Dawar <m.wasifdawar@gmail.com>
2024-01-20 05:46:57 +0500

    Child communication using event

--- force-app/main/default/lwc/childComponentCommunication/childComponentCommunication.js
+++ force-app/main/default/lwc/childComponentCommunication/childComponentCommunication.js
@@ -1,8 +1,16 @@
 import { LightningElement } from 'lwc';
 
 export default class ChildComponentCommunication extends LightningElement {
+    endValue =5;
     handleOnClick()
     {
-        this.dispatchEvent(new CustomEvent("increasecount"));
+        //this.dispatchEvent(new CustomEvent("increasecount"));
+        const myEventWithValue = new CustomEvent("increasecount",{
+            detail:{
+                endValue:  this.endValue,
+                mesg : "Hello From Wasif Dawar"
+            }
+        });
+        this.dispatchEvent(myEventWithValue);
 }
 }
--- force-app/main/default/lwc/parentComponentCommunication/parentComponentCommunication.js
+++ force-app/main/default/lwc/parentComponentCommunication/parentComponentCommunication.js
@@ -2,8 +2,15 @@ import { LightningElement } from 'lwc';
 
 export default class ParentComponentCommunication extends LightningElement {
     count = 1;
-    handleEventChange()
-    {
+    endValue =0;
+    mesg = '';
+    handleEventChange(event)
+    { 
+        this.endValue = event.detail.endValue;
+        this.mesg = event.detail.mesg;
+       if(this.count < this.endValue)
+       {
         this.count = this.count +1;
+       }
     }
 }
