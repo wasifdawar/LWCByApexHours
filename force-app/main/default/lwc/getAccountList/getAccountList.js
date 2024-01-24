94b6e745fb7bc3fb1297b49efcdaf01f4930bd2c
Wasif Dawar <m.wasifdawar@gmail.com>
2024-01-24 23:39:51 +0500

    new Test Component

--- force-app/main/default/lwc/getAccountList/getAccountList.js
+++ force-app/main/default/lwc/getAccountList/getAccountList.js
@@ -1,3 +1,7 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,wire } from 'lwc';
+import getAccList from "@salesforce/apex/AccountController.getAccList";
 
-export default class GetAccountList extends LightningElement {}
+export default class GetAccountList extends LightningElement {
+    @wire(getAccList) accounts;
+    
+}
