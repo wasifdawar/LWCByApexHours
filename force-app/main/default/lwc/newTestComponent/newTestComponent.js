8d2443d493c46ff56e4e540b0914f7acc8da96b3
Wasif Dawar <m.wasifdawar@gmail.com>
2024-01-26 18:09:52 +0500

    ---

--- force-app/main/default/lwc/newTestComponent/newTestComponent.js
+++ force-app/main/default/lwc/newTestComponent/newTestComponent.js
@@ -1,3 +1,8 @@
 import { LightningElement } from 'lwc';
 
-export default class NewTestComponent extends LightningElement {}
+export default class NewTestComponent extends LightningElement {
+    test(event)
+    {
+        
+    } 
+}
